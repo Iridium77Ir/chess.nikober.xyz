82a7ea8c9a1357f5fc168a3a3f13895a1b79cb8d
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-12-07 08:50:05 +0100

    fix cookieParser import bug

--- src/frontend/frontend.js
+++ src/frontend/frontend.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 var app = express();
 const cookieParser = require('cookie-parser');
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use(express.json());
 
