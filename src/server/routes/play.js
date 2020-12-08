4bb5d4de926973ed60745748d246f9f22f402e6b
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-12-08 14:42:41 +0100

    stringify json not parse it
    Signed-off-by: Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>

--- src/server/routes/play.js
+++ src/server/routes/play.js
@@ -51,7 +51,7 @@ function sendError(socket, err) {
 }
 
 function authenticate(token, id) {
-    token = JSON.parse(token);
+    token = JSON.stringify(token);
     var res = jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
         if (err) return {result: false};
         if (data.id != id) return {result: false};
